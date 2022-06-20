53d65b9fa477916eeb65065404f85a8e2381342b
Vir7uaL <wilson.gomes@innowave.tech>
2022-06-20 14:34:30 +0200

    Builder page access to PokemonDataProvider

--- src/Views/pages/builder.jsx
+++ src/Views/pages/builder.jsx
@@ -6,11 +6,13 @@ import PokemonInfo from '../../Components/pokemonInfo';
 import CopyLinkHeader from '../template/header/copyLink';
 import Textarea from '../../Components/forms/textarea';
 
+import { PokemonDataProvider } from '../../Data/Context/pokemonData';
+
 const BuilderPage = (props) => {
     const isLoggedIn = true;
 
     return (
-        <>
+        <PokemonDataProvider pokemonID={69}>
             <CopyLinkHeader />
             <SideInfo classes="col mb-4 mb-lg-0 me-lg-3 pb-3">
                 <PokemonInfo isLoggedIn={isLoggedIn} />
@@ -22,7 +24,7 @@ const BuilderPage = (props) => {
             </SideInfo>
             <Builder />
             <Modal></Modal>
-        </>
+        </PokemonDataProvider>
     );
 }
 export default BuilderPage;
