d7785b73e62f01c6c3482352738ae7ce33d16135
Vir7uaL <wilson.gomes@innowave.tech>
2022-06-20 15:19:56 +0200

    PokemonImg component structer modified

--- src/Components/pokemonImg/pokemonImg.jsx
+++ src/Components/pokemonImg/pokemonImg.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const PokemonImg = (props) => {
     if (props.animated)
         return (
-            <img className={`animated tada ${props.borderType} border-small`} src="assets/img/1_bulbasaur.webp" alt="alt"></img>
+            <img className={`animated tada ${props.borderType} border-small`} src={props.src} alt="alt"></img>
         );
     else
         return (
-            <img className={`${props.borderType} border-small`} src="assets/img/1_bulbasaur.webp" alt="alt"></img>
+            <img className={`${props.borderType} border-small`} src={props.src} alt="alt"></img>
         );
 }
 export default PokemonImg;
