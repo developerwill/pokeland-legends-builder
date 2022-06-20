0612466084aa07631c29616a8d1e95934e16e86f
Vir7uaL <wilson.gomes@innowave.tech>
2022-06-20 14:36:59 +0200

    Builder Header Type with Pokemon Data Context

--- src/Components/headerType/headerType.jsx
+++ src/Components/headerType/headerType.jsx
@@ -1,55 +1,36 @@
-import React, { useContext } from 'react';
-import pokedexDataContext from '../../Data/pokedexDataContext';
-
 import './headerType.css';
+import React, { useContext } from 'react';
+import { PokemonDataContext } from '../../Data/Context/pokemonData';
 
 const HeaderType = (props) => {
-    const { state } = useContext(pokedexDataContext);
-
-    const pokemonInfo = { ...state.filter(pokemon => pokemon.id === props.pokemonID) };
-
-    //Pokemon personal info
-    const type_1 = pokemonInfo[0].type_1;
-    const type_2 = pokemonInfo[0].type_2;
-    const pokedexID = pokemonInfo[0].pokedex_id;
-    const pokelandName = pokemonInfo[0].name;
-
-    //SEO
-    const alt = `Pokeland Legends Builder - ${pokelandName}`;
-    const type_1_alt = `Pokeland Legends Builder - ${type_1}`;
-    const type_2_alt = `Pokeland Legends Builder - ${type_2}`;
-
-    //Path for pictures
-    const picture = 'assets/img/pokedex/pokemon-home/';
-    const typeImg = 'assets/img/header_types/types/';
+    const pokemon = useContext(PokemonDataContext);
 
     return (
-        <div className={`row builder-header rounded-top header-type-color-${type_1}`}>
-
+        <div className={`row builder-header rounded-top header-type-color-${pokemon.types.type_1}`}>
             <div className="col header-type-pokemon-container">
                 <div className='row'>
                     <div className='col-12 pt-2'>
                         <img className="header-type-type"
-                            src={typeImg + type_1 + ".webp"}
-                            alt={type_1_alt}></img>
-                        {type_2 &&
+                            src={pokemon.assets.type_1}
+                            alt={pokemon.seo.type_1_alt}></img>
+                        {pokemon.type_2 &&
                             <img className="header-type-type"
-                                src={typeImg + type_2 + ".webp"}
-                                alt={type_2_alt}></img>
+                                src={pokemon.assets.type_2}
+                                alt={pokemon.seo.type_2_alt}></img>
                         }
                     </div>
                 </div>
 
                 <div className='col-12 mt-3 mt-sm-0'>
-                    {type_2
+                    {pokemon.types.type_2
                         ? <h3 className='text-center text-uppercase fw-bold'>
-                            {type_1 === 'non-type' ? 'non-' : type_1 + ' / ' + type_2 + ' '}type</h3>
+                            {pokemon.types.type_1 === 'non-type' ? 'non-' : pokemon.types.type_1 + ' / ' + pokemon.types.type_2 + ' '}type</h3>
                         :
-                        <h2 className='text-center text-uppercase fw-bold'>{type_1 === 'non-type' ? 'non-' : type_1 + ' '}type</h2>
+                        <h2 className='text-center text-uppercase fw-bold'>{pokemon.types.type_1 === 'non-type' ? 'non-' : pokemon.types.type_1 + ' '}type</h2>
                     }
 
                     <img className="header-type-pokemon float-end"
-                        src={picture + pokedexID + ".webp"} alt={alt}></img>
+                        src={pokemon.assets.avatar} alt={pokemon.seo.alt}></img>
                 </div>
             </div>
         </div>
