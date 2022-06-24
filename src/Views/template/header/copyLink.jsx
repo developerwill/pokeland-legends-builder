c21d018e95e78fcfe3ca68bac1429e0a946ff882
Vir7uaL <wilson.gomes@innowave.tech>
2022-06-24 12:40:35 +0200

    Copy build link to clipboard onClick

--- src/Views/template/header/copyLink.jsx
+++ src/Views/template/header/copyLink.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import HeaderBase from './headerBase';
 import { PokedexContext } from '../../../Data/Context/pokelandApi';
-import { AppConfigContext } from '../../../Config/App';
 
-const CopyLinkHeader = (props) => {
+const CopyLinkHeader = () => {
     const { pokemonData } = useContext(PokedexContext)
-    const { site_url } = useContext(AppConfigContext)
+
+    function copyLink(link) {
+        navigator.clipboard.writeText(link)
+    }
 
     return (
         <HeaderBase>
@@ -15,7 +17,7 @@ const CopyLinkHeader = (props) => {
             <div className="col-12 col-lg-6 ms-md-auto">
                 <div className="input-group" data-bs-toggle="tooltip" data-bs-placement="left" title="Copy &amp; Share!">
                     <input className="form-control custom-border purple-bk" type="text" value={pokemonData.build_url} readOnly="Test"></input>
-                    <button className="btn btn-primary" type="button">Copy &amp; Share!</button>
+                    <button onClick={() => copyLink(pokemonData.build_url)} className="btn btn-primary" type="button">Copy &amp; Share!</button>
                 </div>
             </div>
         </HeaderBase>
