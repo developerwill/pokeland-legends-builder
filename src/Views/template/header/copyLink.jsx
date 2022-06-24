15e20507e97a3908e9fe083eb0046a1345aad3e9
Vir7uaL <wilson.gomes@innowave.tech>
2022-06-24 19:09:24 +0200

    Copy link function with animation!

--- src/Views/template/header/copyLink.jsx
+++ src/Views/template/header/copyLink.jsx
@@ -1,13 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef, useEffect } from 'react';
 import HeaderBase from './headerBase';
 import { PokedexContext } from '../../../Data/Context/pokelandApi';
+import Overlay from 'react-bootstrap/Overlay';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 const CopyLinkHeader = () => {
+    const [show, setShow] = useState(false);
+    const target = useRef(null);
+
     const { pokemonData } = useContext(PokedexContext)
 
     function copyLink(link) {
         navigator.clipboard.writeText(link)
+        setShow(!show)
     }
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShow(false)
+        }, 1500);
+    }, [show]);
 
     return (
         <HeaderBase>
@@ -15,9 +27,17 @@ const CopyLinkHeader = () => {
                 <h1 className="mb-0 site-title box">Pokémon Legends Builder</h1>
             </div>
             <div className="col-12 col-lg-6 ms-md-auto">
-                <div className="input-group" data-bs-toggle="tooltip" data-bs-placement="left" title="Copy &amp; Share!">
+                <div className="input-group tooltipme" data-bs-toggle="tooltip" data-bs-placement="left" title="Copy &amp; Share!">
                     <input className="form-control custom-border purple-bk" type="text" value={pokemonData.build_url} readOnly="Test"></input>
-                    <button onClick={() => copyLink(pokemonData.build_url)} className="btn btn-primary" type="button">Copy &amp; Share!</button>
+
+                    <button onClick={() => copyLink(pokemonData.build_url)} ref={target} className="btn btn-primary" type="button">Copy &amp; Share!</button>
+                    <Overlay target={target.current} show={show} placement="right">
+                        {(props) => (
+                            <Tooltip id="overlay-example" {...props}>
+                                Copied!
+                            </Tooltip>
+                        )}
+                    </Overlay>
                 </div>
             </div>
         </HeaderBase>
