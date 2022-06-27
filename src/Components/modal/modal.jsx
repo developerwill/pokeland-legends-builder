89feb7312647406d7a58574e52f9708441a5f563
Vir7uaL <wilson.gomes@innowave.tech>
2022-06-27 22:19:09 +0200

    Modal dynamicalty shows item list

--- src/Components/modal/modal.jsx
+++ src/Components/modal/modal.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ImgTooltip from '../pokemonImg/imgTooltip';
+import { PokedexContext } from '../../Data/Context/pokelandApi';
 
-const Modal = (props) => {
+const Modal = () => {
+    const { itemList } = useContext(PokedexContext)
+    const itemsListArray = []
+
+    itemList.map((ability_set, i) => (
+        Object.values(ability_set).map(function (name, i) {
+            itemsListArray.push(name)
+        })
+    ))
+
     return (
         <div className="modal fade" role="dialog" tabIndex="-1" id="modal-1">
             <div className="modal-dialog modal-dialog-centered" role="document">
                 <div className="modal-content">
                     <div className="modal-body purple-bk-darker text-center">
-
-                        <ImgTooltip classes="purple-border border-small m-1 img-fluid" src="assets/img/abilities/accuracy.webp" alt="butt"></ImgTooltip>
-                        <ImgTooltip classes="purple-border border-small m-1 img-fluid" src="assets/img/abilities/accuracy.webp" alt="butt"></ImgTooltip>
-
+                        {
+                            itemsListArray.map((name, index) =>
+                                <ImgTooltip key={index} classes="purple-border border-small m-1 img-fluid" src={`assets/img/abilities/${name}`} alt="butt"></ImgTooltip>
+                            )
+                        }
                     </div>
                 </div>
             </div>
