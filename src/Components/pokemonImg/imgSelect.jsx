e0ff18804fa698bddf4ad8e2b1e7f4521f395286
Vir7uaL <wilson.gomes@innowave.tech>
2022-07-04 17:55:32 +0100

    Close on select ability

--- src/Components/pokemonImg/imgSelect.jsx
+++ src/Components/pokemonImg/imgSelect.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { BuildContext } from '../../Data/Context/buildData';
+import { Link } from 'react-router-dom';
 
 /**
  * 
@@ -11,7 +12,10 @@ const ImgSelect = ({ classes, src, alt, item }) => {
     const { updateBuild } = useContext(BuildContext)
 
     return (
-        <img onClick={() => updateBuild(item)} data-bs-toggle="tooltip" data-bss-tooltip="" className={`${classes}`} src={src} title={alt} alt={alt}></img>
+        <Link to="#">
+            <img onClick={() => updateBuild(item)} data-bs-dismiss="modal" data-bs-toggle="tooltip" data-bss-tooltip="" className={`${classes}`} src={src} title={alt} alt={alt}></img>
+        </Link>
+
     );
 }
 export default ImgSelect;
