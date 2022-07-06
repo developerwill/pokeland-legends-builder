7174658c53c030ec28c5581bfccadcafefa4b7c7
Vir7uaL <wilson.gomes@innowave.tech>
2022-07-06 10:59:10 +0100

    Add item btn can receive either Pokemon or Items

--- src/Components/btn/addItemBtn.jsx
+++ src/Components/btn/addItemBtn.jsx
@@ -1,10 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { PokedexContext } from '../../Data/Context/pokemonData';
 import { BuildContext } from '../../Data/Context/buildData';
 
-const AddItemBtn = ({ modal, index, btnIndex }) => {
+const AddItemBtn = ({ modal, index, btnIndex, itemType }) => {
     const { getItemList } = useContext(PokedexContext)
-    const { getBtnKey, buildData } = useContext(BuildContext)
+    const { getBtnKey, buildData, teammates } = useContext(BuildContext)
     const btnStyle = 'btn border rounded-circle d-flex justify-content-center align-items-center add-button box btn-primary'
 
     function getItems(btnIndex) {
@@ -12,13 +12,24 @@ const AddItemBtn = ({ modal, index, btnIndex }) => {
         getBtnKey(btnIndex)
     }
 
-    return (
-        <button onClick={() => getItems(btnIndex)} key={btnIndex} className={btnStyle} type="button" data-bs-toggle="modal" data-bs-target={modal}>
-            {buildData.current && buildData.current.abilities['ability_' + btnIndex]
-                ? <img className='border rounded-circle add-button btn-primary' src={`assets/img/abilities/${buildData.current.abilities['ability_' + btnIndex]}`}></img>
-                : '+'
-            }
-        </button>
-    )
+    if (itemType === 'pokemon') {
+        return (
+            <button onClick={() => getBtnKey(index)} key={btnIndex} className={btnStyle} type="button" data-bs-toggle="modal" data-bs-target={modal}>
+                {teammates && teammates['teammate_' + index]
+                    ? <img className='border rounded-circle add-button btn-primary' alt='alt' src={teammates['teammate_' + index]}></img>
+                    : '+'
+                }
+            </button>
+        )
+    } else {
+        return (
+            <button onClick={() => getItems(btnIndex)} key={btnIndex} className={btnStyle} type="button" data-bs-toggle="modal" data-bs-target={modal}>
+                {buildData.current && buildData.current.abilities['ability_' + btnIndex]
+                    ? <img className='border rounded-circle add-button btn-primary' alt='alt' src={`assets/img/abilities/${buildData.current.abilities['ability_' + btnIndex]}`}></img>
+                    : '+'
+                }
+            </button>
+        )
+    }
 }
 export default AddItemBtn;
