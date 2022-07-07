3ea32ab17a79d2bcba762c924214d979966235be
Vir7uaL <wilson.gomes@innowave.tech>
2022-07-07 11:29:05 +0100

    Language selection added

---
+++ src/Components/dropdown/dropdownDark.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DropDownDark = (props) => {
+    return (
+        <>
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle purple-bk" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                    Language
+                </button>
+
+                <ul className='dropdown-menu dropdown-menu-dark purple-bk'>
+                    <li>
+                        <Link to={'/'} className='dropdown-item d-flex justify-content-between px-2'>Pokeland
+                            <img className='ms-3' alt='alt' src='assets/img/flags/eng.webp'></img>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/'} className='dropdown-item d-flex justify-content-between px-2'>Origem Poke
+                            <img className='ms-3' alt='alt' src='assets/img/flags/pt.webp'></img>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/'} className='dropdown-item d-flex justify-content-between px-2'>Monstrui Origen
+                            <img className='ms-3' alt='alt' src='assets/img/flags/es.webp'></img>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
+}
+export default DropDownDark;
--- src/Views/template/header/homepage.jsx
+++ src/Views/template/header/homepage.jsx
@@ -2,21 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import BtnOffcanvas from '../../../Components/btn/btnOffcanvas';
 import HeaderBase from './headerBase';
+import DropDownDark from '../../../Components/dropdown/dropdownDark';
 
 const HomepageHeader = (props) => {
     return (
-        <HeaderBase>
-            <div className="col-12 col-sm-7 col-md-5 col-xl-4 col-xxl-3 mb-4 mb-sm-0 text-center">
-                <h1 className="mb-0 site-title box">Pokémon Legends Top Builds</h1>
+        <>
+            <div className='d-flex justify-content-end d-md-none'>
+                <DropDownDark />
             </div>
-            <div className="col p-0 d-flex justify-content-end">
-                <BtnOffcanvas classes="btn-primary me-2 d-sm-none" offcanvas="offcanvas-search-1" text="Search Builds" icon="fa-solid fa-plus"></BtnOffcanvas>
+            <HeaderBase>
+                <div className="col-12 col-sm-7 col-md-5 col-xl-4 col-xxl-3 mb-4 mb-sm-0 text-center">
+                    <h1 className="mb-0 site-title box">Pokémon Legends Top Builds</h1>
+                </div>
+                <div className="col p-0 d-flex justify-content-center justify-content-sm-end">
+                    <BtnOffcanvas classes="btn-primary me-2 d-sm-none" offcanvas="offcanvas-search-1" text="Search Builds" icon="fa-solid fa-magnifying-glass"></BtnOffcanvas>
 
-                <Link className='btn btn-success' to={'new-build'}>
-                    Create Build <i className='fa-solid fa-plus'></i>
-                </Link>
-            </div>
-        </HeaderBase>
+                    <div className='d-none d-md-block me-3'>
+                        <DropDownDark />
+                    </div>
+
+                    <Link className='btn btn-success' to={'new-build'}>
+                        Create Build <i className='fa-solid fa-plus'></i>
+                    </Link>
+                </div>
+            </HeaderBase>
+        </>
     );
 }
 export default HomepageHeader;
