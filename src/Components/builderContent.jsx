8f2fe82ef8b2c0ce02b7f9878466aadda5340344
Vir7uaL <wilson.gomes@innowave.tech>
2022-07-14 19:30:03 +0100

    builderContent added the correct names for the modals

--- src/Components/builderContent.jsx
+++ src/Components/builderContent.jsx
@@ -16,12 +16,12 @@ const BuilderContent = (props) => {
                 <InputText placeholder="Type a Pokémon name" name="pokemon-name">Build Title</InputText>
 
                 <ItemRow id="builder-abilities" title="Abilities" itemCategory={'Ability'} numberOfItems={numberOfAbilities} itemType={'ability'} disabled={false} modal="#abilities-modal" />
-                <ItemRow id="builder-personality" title="Personality" itemCategory={'Nature'} numberOfItems={4} itemType={'personality'} disabled={false} modal="#abilities-modal" />
-                <ItemRow id="builder-held-items" title="Held Items" itemCategory={'Item'} numberOfItems={4} itemType={'heldItem'} disabled={false} modal="#abilities-modal" />
-                <ItemRow id="builder-gear" title="Gear" itemCategory={'Gear'} numberOfItems={4} itemType={'gear'} disabled={false} modal="#abilities-modal" />
-                <ItemRow id="builder-hidden-skills" title="Hidden Skills" itemCategory={'Skill'} numberOfItems={12} itemType={'hiddenSkill'} disabled={false} modal="#abilities-modal" />
+                <ItemRow id="builder-personality" title="Personality" itemCategory={'Nature'} numberOfItems={4} itemType={'personality'} disabled={false} modal="#personalities-modal" />
+                <ItemRow id="builder-held-items" title="Held Items" itemCategory={'Item'} numberOfItems={4} itemType={'heldItem'} disabled={false} modal="#heldItems-modal" />
+                <ItemRow id="builder-gear" title="Gear" itemCategory={'Gear'} numberOfItems={4} itemType={'gear'} disabled={false} modal="#gears-modal" />
+                <ItemRow id="builder-hidden-skills" title="Hidden Skills" itemCategory={'Skill'} numberOfItems={12} itemType={'hiddenSkill'} disabled={false} modal="#hiddenSkills-modal" />
 
-                <ItemRow id="options" title="Save and Share" itemType={'saveShare'} modal="#abilities-modal" >
+                <ItemRow id="options" title="Save and Share" itemType={'saveShare'} >
                     <div className='d-flex justify-content-center'>
                         <SaveShare></SaveShare>
                     </div>
