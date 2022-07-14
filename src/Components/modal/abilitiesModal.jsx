2363956bd4cb7a5363ac025d191b3b9b67746ed0
Vir7uaL <wilson.gomes@innowave.tech>
2022-07-14 18:48:11 +0100

    abilitiesModal struture modified

--- src/Components/modal/abilitiesModal.jsx
+++ src/Components/modal/abilitiesModal.jsx
@@ -3,28 +3,28 @@ import { PokedexContext } from '../../Data/Context/pokemonData';
 import ImgSelect from '../pokemonImg/imgSelect';
 
 const AbilitiesModal = () => {
-    const { itemList } = useContext(PokedexContext)
-    const itemsListArray = []
+    const { pokemonAbilities } = useContext(PokedexContext)
+    const pokemonAbilitiesArray = []
 
-    if (itemList.length > 0) {
+    if (pokemonAbilities.length > 0) {
         // eslint-disable-next-line
-        itemList.map(function (item) {
-            itemsListArray.push(item.abilities[0])
+        pokemonAbilities.map(function (item) {
+            pokemonAbilitiesArray.push(item.abilities[0])
         })
     }
 
     // eslint-disable-next-line
-    Object.values(itemsListArray).map(function (item) {
-        itemsListArray.push(item)
+    Object.values(pokemonAbilitiesArray).map(function (item) {
+        pokemonAbilitiesArray.push(item)
     })
 
     return (
         <div className="modal fade" role="dialog" tabIndex="-1" id="abilities-modal">
             <div className="modal-dialog modal-dialog-centered" role="document">
                 <div className="modal-content">
-                    <div className="modal-body purple-bk-darker text-center">
-                        {itemsListArray.length > 0 &&
-                            Object.values(itemsListArray[0]).map(function (item, index) {
+                    <div className="modal-body purple-bk-darker d-flex flex-wrap justify-content-center">
+                        {pokemonAbilitiesArray.length > 0 &&
+                            Object.values(pokemonAbilitiesArray[0]).map(function (item, index) {
                                 return (
                                     <ImgSelect key={index + 1} item={item} itemIndex={index + 1} classes="purple-border border-small m-1 img-fluid img-select" src={`assets/img/abilities/${item}`} alt="butt"></ImgSelect>
                                 )
