29f26ae34f1996e217d5c31382bf8853715f7ec0
Vir7uaL <wilson.gomes@innowave.tech>
2022-07-15 19:11:30 +0100

    Build title added to the buildData

--- src/Components/builderContent.jsx
+++ src/Components/builderContent.jsx
@@ -1,19 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import ItemRow from './itemRow/itemRow';
 import HeaderType from './headerType/headerType';
 import InputText from './forms/inputText';
 import { PokedexContext } from '../Data/Context/pokemonData';
 import SaveShare from './btn/buildStatus';
+import { BuildContext } from '../Data/Context/buildData';
 
 const BuilderContent = (props) => {
     const { pokemonData } = useContext(PokedexContext);
+    const { updateBuildTitle } = useContext(BuildContext);
+    const [buildTitle, setBuildTitle] = useState();
     const numberOfAbilities = pokemonData.numberOf.abilities;
+
+    useEffect(() => {
+        updateBuildTitle(buildTitle)
+        // eslint-disable-next-line
+    }, [buildTitle])
 
     return (
         <div className="col-lg-7 col-xl-8 col-xxl-9 custom-border rounded-3">
             <HeaderType />
             <form className='form-builder'>
-                <InputText placeholder="Type a Pokémon name" name="pokemon-name">Build Title</InputText>
+                <InputText setBuildTitle={setBuildTitle} placeholder="Type a Pokémon name" name="pokemon-name">Build Title</InputText>
 
                 <ItemRow id="builder-abilities" title="Abilities" itemCategory={'Ability'} numberOfItems={numberOfAbilities} itemType={'ability'} disabled={false} modal="#abilities-modal" />
                 <ItemRow id="builder-personality" title="Personality" itemCategory={'Nature'} numberOfItems={4} itemType={'personality'} disabled={false} modal="#personalities-modal" />
